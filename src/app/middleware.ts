387c36c0163fbfcdfb43dc71a6e795fb0594ade5
arya-summitglobal <aryapdf@users.noreply.github.com>
2025-10-31 16:16:44 +0700

    update page

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,12 +1,16 @@
-import {NextRequest, NextResponse} from "next/server";
+// middleware.ts
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-    const isAdminRoute = req.nextUrl.pathname.startsWith('/admin');
-    const userToken = req.cookies.get('auth_token')?.value;
+    const isAdminRoute = req.nextUrl.pathname.startsWith("/(admin)");
+    const isLoggedIn = req.cookies.get("session_token");
 
-    if (isAdminRoute && !userToken) {
-        return NextResponse.redirect(new URL('/login', req.url))
+    if (isAdminRoute && !isLoggedIn) {
+        return NextResponse.redirect(new URL("/login", req.url));
     }
-
-    return NextResponse.next();
 }
+
+export const config = {
+    matcher: ["/(admin)/:path*"],
+};
