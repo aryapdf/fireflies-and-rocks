38022d2980ff66e1c7b7cc965b71c3c293f626d5
aryapdf <aryaagnipradana@gmail.com>
2026-01-08 13:29:49 +0700

    complete project queries

--- src/lib/db/queries.ts
+++ src/lib/db/queries.ts
@@ -40,6 +40,42 @@ export async function getProjectById(id: String) {
         .eq("id", id)
         .single()
     
-    if (error) throw new Error(`Failed to fetch projects: ${error.message}`);
+    if (error) throw new Error(`Failed to fetch project: ${error.message}`);
+    return data as Project;
 }
+
+
+export async function createProject(project: Omit<Project, "id" | "created_at">) {
+    const { data, error } = supabase
+        .from("projects")
+        .insert(project)
+        .select()
+        .single()
+
+    if (error) throw new Error(`Failed to create new project: ${error.message}`);
+    return data as Project;
+}
+
+export async function updateProject(id: string, updates: Partial<Omit<Project, "id">>){
+    const { data, error } = supabase 
+        .from("projects")
+        .eq("id", id)
+        .select()
+        .single();
+
+    if (error) throw new Error(`Failed to update project: ${error.message}`);
+    return data as Project;
+}
+
+export async function deleteProject(id:string) {
+    const {error} = supabase
+        .from("projects")
+        .delete()
+        .eq("id", id)
+        
+    if (error) throw new Error(`Failed to delete project: ${error.message}`);
+    return { success: true };
+}
+
+// EXPERIENCES
 
