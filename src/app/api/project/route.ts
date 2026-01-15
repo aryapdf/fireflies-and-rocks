1f67eb690cdaea8e4cccd529cb5f93999b5cd31a
aryapdf <aryaagnipradana@gmail.com>
2026-01-15 15:26:30 +0700

    modfiy project routes

--- src/app/api/project/route.ts
+++ src/app/api/project/route.ts
@@ -1,12 +1,18 @@
 import { NextRequest } from "next/server";
-import { getProjects, createProject } from "@/lib/db/queries";
-import { projectSchema } from "@/lib/validations/portfolio";
+import { getProjects, createProject, getProjectById, updateProject, deleteProject } from "@/lib/db/queries";
+import { projectSchema, updateProjectSchema } from "@/lib/validations/portfolio";
 import { successResponse, errorResponse, validationErrorResponse } from "@/lib/utils/api-response";
 
 export async function GET(request:NextRequest) {
     try {
         const searchParams = request.nextUrl.searchParams;
         const featured = searchParams.get("featured");
+        const projectId = searchParams.get("projectId");
+
+        if (projectId) {
+            const project = await getProjectById(projectId)
+            return successResponse(project)
+        }
 
         let featuredFilter: boolean | undefined;
 
@@ -38,5 +44,41 @@ export async function POST(request:NextRequest) {
     } catch (error:any) {
         console.error("Error creating any project", error)
         return errorResponse(error.message || "Failed creating any project");
+    }
+}
+
+export async function PUT(request:NextRequest) {
+    try {
+        const body = await request.json();
+        const validation:any = updateProjectSchema.safeParse(body)
+        const searchParams = request.nextUrl.searchParams;
+
+        if (!validation.success) {
+            return validationErrorResponse(validation.error.format())
+        }
+        
+        const projectId = searchParams.get("projectId");
+
+        if (!projectId) {
+            throw new Error("Project ID is required.")
+        }
+
+        const project = await updateProject(projectId, validation.data)
+        return successResponse(project);
+    } catch (error:any) {
+        console.error("Error updating any project", error)
+        return errorResponse(error.message || "Failed creating any project");
+    }
+}
+
+export async function DELETE(request:NextRequest){
+    try {
+        const searchParams = request.nextUrl.searchParams;
+        const projectId:any = searchParams.get("projectId")
+        await deleteProject(projectId)
+        return successResponse({ message: "Project deleted successfully" })
+    } catch (error:any) {
+        console.error("Error deleting any project", error)
+        return errorResponse(error.message || "Failed to delete project")
     }
 }
